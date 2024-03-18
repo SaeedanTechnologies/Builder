1b72df250bcbab00e0c2ad896305d2ff12b15869
Daniyal1303 <daniyalahmad1303@gmail.com>
2024-03-18 13:02:20 +0500

    Comonent is deleted and copied

--- src/components/ui/Element.jsx
+++ src/components/ui/Element.jsx
@@ -3,31 +3,37 @@ import { useRef } from "react";
 
 const Element = ({ children, titleClass, title }) => {
   const divElement = useRef();
+
+  const handleDelete = () => {
+    divElement.current.remove();
+  };
+
+  const handleDuplicate = () => {
+    const htmlElement = divElement.current;
+    if (htmlElement) {
+      const clonedElement = htmlElement.cloneNode(true);
+      htmlElement.insertAdjacentElement("afterend", clonedElement);
+    }
+  };
+
   return (
     <div
       className="group relative outline-none"
       contentEditable={true}
       suppressContentEditableWarning={true}
       ref={divElement}
+      draggable
     >
       <div className="absolute inset-0 hidden border-2 border-blue-600 group-focus-within:block">
         <div className="absolute -translate-y-full pl-2">
           <div className="flex items-center gap-x-2 rounded-t-lg bg-blue-600 px-3 py-1 text-white cursor-pointer ">
             <span className={titleClass}>{title}</span>
 
-            <span
-              onClick={() => {
-                divElement.current.remove();
-              }}
-            >
+            <span className="delete-button" onClick={handleDelete}>
               <IconDelete className="h-4 w-4 fill-current" />
             </span>
 
-            <span
-              onClick={() => {
-                alert("copy");
-              }}
-            >
+            <span className="duplicate-button" onClick={handleDuplicate}>
               <IconDuplicateOutline className="h-4 w-4 fill-current" />
             </span>
           </div>
